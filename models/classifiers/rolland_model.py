ede9f91c1f1f1d9f625a563dac4fd6f9dc09075d
Matheus Viana <matheus_o_viana@hotmail.com>
2025-11-12 12:57:13 +0100

    clean up rolands model call in the main notebook

--- models/classifiers/rolland_model.py
+++
@@ -1,5 +0,0 @@
-def rolland_model(final_df): 
-  # the name do the dataframe is final_df in the main.ipynb
-  # You should work with the df parameter the DF parameter
-  # is already a dataframe. When you import 
-  pass
